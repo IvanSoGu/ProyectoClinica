16d5d2b3f631e29c691a452effa4606e921870a1
IvanSoGu <Ivansogu@outlook.com>
2022-05-31 09:01:27 +0200

    Arreglado filtro

--- src/main/java/com/softtek/servicio/impl/AuthServiceImp.java
+++ src/main/java/com/softtek/servicio/impl/AuthServiceImp.java
@@ -1,48 +1,33 @@
 package com.softtek.servicio.impl;
-
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
 @Service
 public class AuthServiceImp {
-
-	public boolean tieneAcceso(String path) {
-		
-		boolean rpta = false;
-		
-		String metodoRol = "" ;
-		
-		switch(path) {
-		case "listar":
-			metodoRol="ADMIN";
-			break;
-			
-		case"listarPorId":
-			metodoRol="ADMIN,USER,DBA";
-			break;
-		}
-		
-		String metodoRoles[] = metodoRol.split(",");
-		
-		Authentication usuarioLogueado = SecurityContextHolder.getContext().getAuthentication();
-		
-		System.out.println(usuarioLogueado.getName());
-		
-		for (GrantedAuthority auth : usuarioLogueado.getAuthorities()) {
-			String rolUser = auth.getAuthority();
-			System.out.println(rolUser);
-			
-			for(String rolMet : metodoRoles) {
-				if (rolUser.equalsIgnoreCase(rolMet)) {}
-				rpta = true;
-			}
-			
-		}
-		
-		return rpta;
-		
-	}
-	
+    public boolean tieneAcceso(String path) {
+        boolean rpta = false;
+        String metodoRol = "";
+        switch (path) {
+        case "listar":
+            metodoRol = "ADMIN";
+            break;
+        case "listarId":
+            metodoRol = "ADMIN,USER,DBA";
+            break;
+        }
+        String metodoRoles[] = metodoRol.split(",");
+        Authentication usuarioLogeado = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(usuarioLogeado.getName());
+        for (GrantedAuthority auth : usuarioLogeado.getAuthorities()) {
+            String rolUser = auth.getAuthority();
+            System.out.println(rolUser);
+            for (String rolMet : metodoRoles) {
+                if (rolUser.equalsIgnoreCase(rolMet)) {
+                    rpta = true;
+                }
+            }
+        }
+        return rpta;
+    }
 }
